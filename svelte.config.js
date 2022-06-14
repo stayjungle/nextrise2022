be9bcd484f3f2c95d8e26dcfd4501852cfb7db34
ak-stayjungle <ak@stayjungle.com>
2022-06-14 20:15:45 +0900

    feat: 빈 sveltekit 프로젝트

---
+++ svelte.config.js
@@ -0,0 +1,40 @@
+import adapter from '@sveltejs/adapter-static'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: {
+        configFilePath: path.join(__dirname, 'postcss.config.js'),
+      },
+    }),
+  ],
+
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true,
+    },
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('./src/components'),
+        },
+      },
+      test: {
+        globals: true,
+        environment: 'jsdom',
+      },
+    },
+  },
+}
+
+export default config
